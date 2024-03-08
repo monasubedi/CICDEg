971d1cd8ded9b6676259985aed3a12926cc8e6b3
monasubedi <sbd.mona@gmail.com>
2024-03-08 17:27:09 -0600

    update

---
+++ App.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React from 'react'
+import Crashes from 'appcenter-crashes'
+import Analytics from 'appcenter-analytics'
+
+const App = () => {
+    return (
+        <View style={styles.center}>
+            <Button title='Track event' onPress={() => Analytics.trackEvent('test')} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+})
+
+export default App
